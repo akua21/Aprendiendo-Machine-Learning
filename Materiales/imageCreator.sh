c253f0505633d96a4f1ed9abcfa2be91534a61cc
pheithar <pheithar@hotmail.com>
2020-03-19 16:47:04 +0100

    imageCreator.sh

--- Materiales/imageCreator.sh
+++ Materiales/imageCreator.sh
@@ -1,9 +1,9 @@
 # !/ bin / bash
 
 #Comprueba que solo haya 1 argumento
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-  echo "-ERROR- : Too many/not enough arguments were given, required 1 FILE"
+  echo "-ERROR- : Too many/not enough arguments were given, required 2 FILE: origin, dest"
   exit 0
 fi
 
@@ -16,3 +16,44 @@ then
   echo "$directory is not a directory"
   exit 0
 fi
+
+src=$2
+
+#Si no es un directorio
+if ! [[ -d $src ]]
+then
+  echo "$src is not a directory"
+  exit 0
+fi
+
+
+i=0
+
+name="flower_"
+count=1
+termination=".jpg"
+
+#Recorrer todos los archivos del directorio pasado por parametro
+for file in "$directory"/*
+do
+
+  fullname="${src}/${name}${count}${termination}"
+
+  #convert  $file  -channel G -level 25%,100%,.6 $fullname
+  convert $file +dither -colors 32 $fullname
+  convert $fullname +dither -posterize 3 $fullname
+  convert $fullname -dither None -colors 64 $fullname
+  convert $file +dither -colors 32 $fullname
+  convert $fullname -separate -threshold 70% -combine $fullname
+  convert $fullname -blur 0x3 -paint 10 $fullname
+  convert $fullname -filter Gaussian -blur 0x8 $fullname
+  convert $fullname -filter Gaussian -blur 0x8 $fullname
+  convert $fullname -filter Gaussian -resize 10% -define filter:sigma=2.5 -resize 1000%  $fullname
+
+
+  echo $fullname
+
+  ((count++))
+
+
+done
